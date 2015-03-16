00262c0e0f3bde6afad8ae5f7be6e16195a46743
Chuck Pierce <chuckmpierce@gmail.com>
2015-03-16 13:07:35 -0400

    drew pointers for things

--- purse.js
+++ purse.js
@@ -40,14 +40,44 @@ function drawCanvas(ctx) {
 		}
 	}
 
-	function drawData(context) {
+	function drawData(str, x, y, context) {
+		ctx.font="12px Tahoma";
+		ctx.fillText(str, x, y);
+	}
 
+	function drawBoldData (str, x, y, context) {
+		ctx.font="bold 12px Tahoma";
+		ctx.fillText(str, x,y);
+		
+	}
+
+	function drawLine(context, x, y) {
+		context.beginPath();
+		context.moveTo(x, y);
+		context.lineTo(400, y);
+		context.stroke();
 	}
 
 	drawDot(485,200, ctx);
 	drawDot(440,310, ctx);
 	drawDot(425,240, ctx);
-	ctx.fillText('Weight 0.98 Pounds (445 G)', 20, 20);
+
+	drawData('WEIGHT', 10, 20, ctx);
+	drawBoldData('0.98 Pounds (445 G)', 75, 20);
+	
+	drawData('LEATHER', 40, 225, ctx);
+	drawBoldData('GOLD DYED PYTHON', 100, 225);
+	drawLine(ctx, 260, 215);
+
+	drawData('BUCKLE', 25, 265, ctx);
+	drawBoldData('RHODIUM-PLATED BRASS', 75, 265);
+	drawLine(ctx, 260, 260);
+
+	drawBoldData('HEAVY GUAGE NYLON STITCH', 55, 335);
+	drawLine(ctx, 260, 325);
+
+	drawData('INSIDE', 155, 400, ctx);
+	drawBoldData('SUEDE LINING', 200, 400);
 }
 
 drawCanvas(ctx);
