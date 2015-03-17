9e8249bfdf6d71b3ebde690285a17aa760a1214e
Chuck Pierce <chuckmpierce@gmail.com>
2015-03-16 23:05:45 -0400

    finished drawing all lines and dimensions

--- purse.js
+++ purse.js
@@ -51,10 +51,17 @@ function drawCanvas(ctx) {
 		
 	}
 
-	function drawLine(context, x, y) {
+	function drawLine(context, x, y, l) {
 		context.beginPath();
 		context.moveTo(x, y);
-		context.lineTo(400, y);
+		context.lineTo(l, y);
+		context.stroke();
+	}
+
+	function drawVertical(context, x, y, l) {
+		context.beginPath();
+		context.moveTo(x, y);
+		context.lineTo(x, l);
 		context.stroke();
 	}
 
@@ -67,17 +74,37 @@ function drawCanvas(ctx) {
 	
 	drawData('LEATHER', 40, 225, ctx);
 	drawBoldData('GOLD DYED PYTHON', 100, 225);
-	drawLine(ctx, 260, 215);
+	drawLine(ctx, 260, 215, 400);
 
 	drawData('BUCKLE', 25, 265, ctx);
 	drawBoldData('RHODIUM-PLATED BRASS', 75, 265);
-	drawLine(ctx, 260, 260);
+	drawLine(ctx, 260, 260, 400);
 
 	drawBoldData('HEAVY GUAGE NYLON STITCH', 55, 335);
-	drawLine(ctx, 260, 325);
+	drawLine(ctx, 260, 325, 400);
 
 	drawData('INSIDE', 155, 400, ctx);
 	drawBoldData('SUEDE LINING', 200, 400);
+
+	drawBoldData('7 3/4 INCHES', 560, 80);
+	drawBoldData('(197 MM)', 570, 95);
+	drawLine(ctx, 455, 120, 750);
+	drawVertical(ctx, 455, 90, 160);
+	drawVertical(ctx, 750, 90, 160);
+
+	drawBoldData('7 1/2 INCHES', 800, 300);
+	drawBoldData('(192 MM)', 810, 315);
+	drawLine(ctx, 880, 180, 800);
+	drawLine(ctx, 880, 420, 800);
+	drawVertical(ctx, 838, 180, 250);
+	drawVertical(ctx, 838, 350, 420);
+
+	drawBoldData('1 3/4 INCHES', 950, 80);
+	drawBoldData('(26 MM)', 965, 95);
+	drawLine(ctx, 1025, 140, 960);
+	drawVertical(ctx, 1025, 120, 160)
+	drawVertical(ctx, 960, 120, 160)
+
 }
 
 drawCanvas(ctx);
